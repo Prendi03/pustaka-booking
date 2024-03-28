f057e47943fb6543bac68644452b6fffafa1ff88
Prendi03 <eprenditakupa@gmail.com>
2024-03-28 12:36:47 +0700

    tugas1

--- application/controllers/tugas1.php
+++ application/controllers/tugas1.php
@@ -1,2 +1,12 @@
 <?php
- 
+ class tugas1 extends CI_Controller
+ {
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"nama Saya Eprendi
+        Saya tinggal di sui jawi 
+        olahraga yang saya sukai adalah 
+        sepak bola";
+    }
+ }
