b6caeaa411598bb9e2278efedd05a330f37e594d
Prendi03 <eprenditakupa@gmail.com>
2024-04-24 11:05:04 +0700

    tugas 4 pertemuan 5

---
+++ application/views/v_about.php
@@ -0,0 +1,31 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li >Nama : eprendi</li>
+        <li>Umur           : 21TH</li>
+        <li>Jenis Kelamin  : Pria</li>
+        <li>Mahasiswa</li>
+       
+    </ul>
+    <h4>Riwayat Sekolah</h4>
+    <li>SDN 10 SEBADOK</li>
+    <li>SMP NEGERI 03 JELIMPO</li>
+    <li>SMA SANTO BENEDIKTUS PAHAUMAN </li>
+    <li>UNIVERSITAS BINA SARANA INFORMATIKA</li>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan PANCASILA GG.WASPADA 1</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>SANGGAU</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>BOLAKAKI</li>
+        <li>BADMINTON</li>
+    </ul>
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<footer>
+                 <a href="http://www.RentalBuku.com">Aether</a>
+            </footer>
+        </div>
+</body>
+</html>
